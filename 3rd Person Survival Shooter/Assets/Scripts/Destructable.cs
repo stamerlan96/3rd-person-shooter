f8fb78173e07fcf835b630d3f5b88f97c8397ba3
stamerlan96 <112584282+stamerlan96@users.noreply.github.com>
2022-08-31 23:45:41 +0300

    Deleted NoNeeds Scripts

--- 3rd Person Survival Shooter/Assets/Scripts/Destructable.cs
+++ 3rd Person Survival Shooter/Assets/Scripts/Destructable.cs
@@ -6,7 +6,7 @@ public class Destructable : MonoBehaviour
 
     [SerializeField] private float startsHitsPoints = 100f;
     [SerializeField] private float hitPoints;
-    // Start is called before the first frame update
+
     void Start()
     {
         hitPoints = startsHitsPoints;
